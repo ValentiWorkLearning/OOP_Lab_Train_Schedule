1a0bd2aa7d1c70888ee42b04d84b347ccb9329c1
Valentyn Korniienko <kornienko-vr@rambler.ru>
2018-01-18 02:54:14 +0200

    Tab fixes

--- TrainScheduleItem.cpp
+++ TrainScheduleItem.cpp
@@ -2,11 +2,11 @@
 #include "Station.hpp"
 
 TrainScheduleItem::TrainScheduleItem(   Station & _station              , 
-										Date  _comingTime               , 
-										Date  _incomingTime):	
-										m_station(_station)             , 
-										m_arrivalTime(_comingTime)      ,
-										m_departureTime(_incomingTime)
+                                        Date  _comingTime               , 
+                                        Date  _incomingTime):	
+                                        m_station(_station)             , 
+                                        m_arrivalTime(_comingTime)      ,
+                                        m_departureTime(_incomingTime)
 {
 	if (m_arrivalTime > m_departureTime)
 		throw std::logic_error(Messages::InvalidTimeSended);
