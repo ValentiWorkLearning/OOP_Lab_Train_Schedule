e3888198c6750bb0dbee0ec5fd1072792ffbfa7a
Valentyn Korniienko <kornienko-vr@rambler.ru>
2018-01-18 02:53:07 +0200

    Tab fixes

--- TrainScheduleItem.cpp
+++ TrainScheduleItem.cpp
@@ -1,11 +1,11 @@
 #include "TrainScheduleItem.hpp"
 #include "Station.hpp"
 
-TrainScheduleItem::TrainScheduleItem(	Station & _station				, 
-										Date  _comingTime				, 
+TrainScheduleItem::TrainScheduleItem(   Station & _station              , 
+										Date  _comingTime               , 
 										Date  _incomingTime):	
-										m_station(_station)				, 
-										m_arrivalTime(_comingTime)		,
+										m_station(_station)             , 
+										m_arrivalTime(_comingTime)      ,
 										m_departureTime(_incomingTime)
 {
 	if (m_arrivalTime > m_departureTime)
